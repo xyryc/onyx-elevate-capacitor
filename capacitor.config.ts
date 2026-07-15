bf22c6578702eb5b507787b00111fe6f6233e50a
xyryc <amdanikzz@gmail.com>
2026-07-15 13:57:16 +0600

    feat: added capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.onyxelevate.app',
+  appName: 'Onyx Elevate',
+  webDir: 'dist'
+};
+
+export default config;
